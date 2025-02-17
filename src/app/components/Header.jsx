a131145dd5ba25437b34bd4258c38c81c31fd44e
aduonye jaja <aduonyeibifubara@gmail.com>
2025-02-17 18:33:22 +0100

    created header component

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex justify-between items-center bg-slate-800 text-white py-4 px-4">
+        <div className="text-4xl">
+          <Link href="/">Clerk Authentication HomePage</Link>
+        </div>
+        <div className="flex justify-between gap-2">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/sign-in">Sign In</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
